f6c8d35cde834224f5288f23ed259a6b105389c8
Ding NING <ning.di@northeastern.edu>
2024-01-12 05:32:06 +1300

    Add files via upload

--- train.py
+++ train.py
@@ -20,7 +20,7 @@ models_path = 'configs/'
 out_path = 'out/'
 
 node_feat_filename = 'node_feats_ssta_1980_2010.npy'
-adj_filename = 'adj_mat_0.8.npy'
+adj_filename = 'adj_mat_0.9.npy'
 
 window_size = 12
 lead_time = 3
